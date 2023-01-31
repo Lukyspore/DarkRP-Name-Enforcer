3d7dc40392be909f6c149fa36555c0d152fcb0a8
Bloodaxe <36518086+Bloodaxe95@users.noreply.github.com>
2023-01-31 08:55:27 +0100

    Added more language/text customization options

--- darkrp_name_enforcer/lua/rpname_enforcer/sh_config.lua
+++ darkrp_name_enforcer/lua/rpname_enforcer/sh_config.lua
@@ -1,6 +1,15 @@
 EnforcerConfig = EnforcerConfig or {}
 
 EnforcerConfig.CommunityName = "You community name" -- Name of your community
+EnforcerConfig.Title = "Welcome to the server!" -- Title top text
+EnforcerConfig.DoneButton = "Done" -- Text in the done-buton
+EnforcerConfig.EnterNameLabel = "Below you must enter a roleplay name for your character." -- Tooltip for describing what to do
+EnforcerConfig.EnterFirstNameLabel = "First Name:" -- The label describing the "first name" field
+EnforcerConfig.FirstNameTextEntry = "First Name" -- Default text displayed in the "first name" box
+EnforcerConfig.EnterLastNameLabel = "Last Name:" -- The label describing the "last name" field
+EnforcerConfig.LastNameTextEntry = "Last Name" -- Default text displayed in the "last name" box
+
+
 EnforcerConfig.FirstNameMaxChars = 12 -- Maximum lenght of First Name
 EnforcerConfig.LastNameMaxChars = 12 -- Maximum lenght of Last Name
 EnforcerConfig.AllowNumbers = false -- Should numbers be allowed
